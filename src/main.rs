d9219d872ec89be4e1c204ad66c603c58bd0a233
faliszewskii <igor.faliszewski1@gmail.com>
2026-06-06 23:35:17 +0200

    feat: Add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+
+use clap::Parser;
+use std::path::PathBuf;
+use log::trace;
+
+const PROGRAM_NAME: &str = "C compiler in Rust";
+const PROGRAM_DESC: &str = "A C language compiler written in Rust.";
+
+#[derive(Parser, Debug)]
+#[command(version, name = PROGRAM_NAME, about = PROGRAM_DESC)]
+struct Args {
+    /// Path to input C source files to compile
+    inputs: Vec<PathBuf>,
+
+    /// Path to the output executable
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args = Args::parse();
+    trace!("Parsed the following args: {:?}", args);
 }
